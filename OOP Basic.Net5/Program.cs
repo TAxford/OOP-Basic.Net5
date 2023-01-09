e31942bdcbfe0d81743c0761733b7f909def64c1
Timothy Axford <t.axford2@yahoo.com>
2023-01-08 20:16:33 -0500

    Added Concole.Readkey to display on the console

--- OOP Basic.Net5/Program.cs
+++ OOP Basic.Net5/Program.cs
@@ -9,8 +9,14 @@ namespace OOP_Basic.Net5
             //create an onject of my class
             //an instance of Human
             Human tim = new Human();
+
+            //access public variable from outside, and even change it
             tim.firstName = "Tim";
+            //call methods of the class
             tim.IntroduceMyself();
+
+
+            Console.ReadKey();
         }
     }
 }
